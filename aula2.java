c59907ddf89c071df42c049f2210d7e828bc525c
Andrews Nícolas <pacificonicolas77@gmail.com>
2026-02-09 20:48:28 -0300

    Primeiro dia

---
+++ aula2.java
@@ -0,0 +1,16 @@
+package com.mycompany.mavenproject1.Aula2;
+
+public class aula2 {
+    public static void main(String a[]){
+        /**
+        System.out.println(5+7);
+        System.out.println(3+9);
+        */
+        int num = 4;
+        System.out.println(num+8);
+        
+        int num1 = 6;
+        int result = num + num1;
+        System.out.print(result);
+    }
+}
