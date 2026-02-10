481b1534ce6dc06e9469980a915b47c9698aebbb
Andrews Nícolas <pacificonicolas77@gmail.com>
2026-02-10 15:44:33 -0300

    Definitiva aula2

--- Ex06.java
+++ Ex06.java
@@ -1,4 +1,23 @@
 package com.mycompany.mavenproject1.Aula2;
+import java.lang.Math.*;
+import java.util.Scanner;
 
 public class Ex06 {
+    public static void main(String a[]){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o seu peso");
+        float peso = leitor.nextFloat();
+
+        System.out.println("Digite a sua altura em metros");
+        float altura = leitor.nextFloat();
+
+        leitor.close();
+
+        float divisor = (float) Math.pow(altura,2);
+        float imc = peso/divisor;
+
+        System.out.printf("O seu índice de massa corporal é : %.2f", imc);
+
+    }
 }
--- Ex11.java
+++ Ex11.java
@@ -1,4 +1,22 @@
 package com.mycompany.mavenproject1.Aula2;
-
+import java.math.BigInteger;
+import java.lang.Math;
+import java.util.Scanner;
 public class Ex11 {
+    public static void main(String a[]){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Digite um número");
+        int num1 = reader.nextInt();
+        System.out.println("Digite outro número");
+        int num2 = reader.nextInt();
+        reader.close();
+        int res1 = num1*num2;
+        BigInteger bg1 = BigInteger.valueOf(num1);
+        BigInteger bg2 = BigInteger.valueOf(num2);
+        BigInteger res2 = BigInteger.valueOf(num2);
+
+        System.out.println(res1);
+        System.out.println(res2);
+        //Deu negativo? Overflow
+    }
 }
