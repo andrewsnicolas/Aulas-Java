810da83073dc139853046f902ad5e90ecb97b763
Andrews Nícolas <pacificonicolas77@gmail.com>
2026-03-13 22:03:31 -0300

    Exercicios de vetores parte 3

--- Aula8/Vetores/Ex45.java
+++ Aula8/Vetores/Ex45.java
@@ -1,19 +1,21 @@
 package Aulas.Aula8.Vetores;
 import java.util.Scanner;
-import java.util.Arrays;
 public class Ex45 {
     public static void main(String args[]){
         Scanner reader = new Scanner(System.in);
-        int[] vetor = new int[6];
-        int aux;
+        int[] vetor = new int[8];
+        int aux1, aux2;
         for(int c = 0; c < vetor.length; c++){
             System.out.println("Digite um valor para o vetor");
             vetor[c] = reader.nextInt();
-            if(c>0 && c<(vetor.length-1)){
-                if(vetor[c]<vetor[c+1]){
-                    aux = vetor[c+1];
-                    vetor[c+1] = vetor[c];
-                    vetor[c] = aux;
+            if(c>0){
+                for(int i=c; i>0; i--){
+                    if(vetor[i]<vetor[i-1]){
+                        aux1=vetor[i];
+                        aux2=vetor[i-1];
+                        vetor[i-1] = aux1;
+                        vetor[i] = aux2;
+                    }
                 }
             }
         }
@@ -21,4 +23,3 @@ public class Ex45 {
         for(int c = 0; c<vetor.length;c++) System.out.print(vetor[c]+" ");
     }
 }
-
